6afa54657118122740433675aa870e506a6ee2c3
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-08-31 12:25:06 +0400

    Initial commit

---
+++ app/src/main/java/net/coremotion/challenge1/ui/users/source/UsersAdapter.kt
@@ -0,0 +1,54 @@
+package net.coremotion.challenge1.ui.users.source
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import net.coremotion.challenge1.databinding.ItemUserBinding
+import net.coremotion.challenge1.domain.model.Users
+import net.coremotion.challenge1.extensions.setImage
+
+typealias UserItemOnClick = (id: Int) -> Unit
+
+class UsersAdapter() : PagingDataAdapter<Users.Data, UsersAdapter.UserViewHolder>(
+    DiffCallback()
+) {
+
+    var userItemOnClick: UserItemOnClick? = null
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.onBind()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = UserViewHolder(
+        ItemUserBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    )
+
+    inner class UserViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind() {
+            val user = getItem(bindingAdapterPosition)
+            binding.profileImage.setImage(user?.avatar)
+            binding.tvName.text = user?.firstName.toString().plus(" ").plus(user?.lastName.toString())
+            binding.tvEmail.text = user?.email.toString()
+            binding.root.setOnClickListener {
+                user?.id?.let { it -> userItemOnClick?.invoke(it) }
+            }
+        }
+    }
+}
+
+class DiffCallback : DiffUtil.ItemCallback<Users.Data>() {
+    override fun areItemsTheSame(oldItem: Users.Data, newItem: Users.Data) =
+        oldItem.id == newItem.id
+
+
+    override fun areContentsTheSame(oldItem: Users.Data, newItem: Users.Data): Boolean {
+        return oldItem == newItem
+    }
+
+
+}
